8721d675fe3603e87370d678a6c309ecf547f4ef
ashish kodam <ashish.kodam@gmail.com>
2023-09-29 09:38:16 -0500

    List_ScrollView

--- App.js
+++ App.js
@@ -10,28 +10,32 @@ import {
 } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Ashish");
-  const [age, setAge] = useState("30");
+  const [people, setPeople] = useState([
+    { name: "shaun", key: "1" },
+
+    { name: "yoshi", key: "2" },
+
+    { name: "mario", key: "3" },
+
+    { name: "luigi", key: "4" },
+    { name: "peach", key: "5" },
+
+    { name: "toad", key: "6" },
+
+    { name: "bowser", key: "7" },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder="Enter the name"
-        onChangeText={(val) => setName(val)}
-      />
-      <Text>Enter Age:</Text>
-      <TextInput
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder="Enter the Age"
-        onChangeText={(val) => setAges(val)}
-      />
-      <Text>
-        Name:{name}, age:{age}
-      </Text>
+      <ScrollView>
+        {people.maps((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={style.item}>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -40,14 +44,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
